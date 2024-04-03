30b8862fdcddfe45a67f53767cde75c4feb2bf20
Rich <rickygb082002@gmail.com>
2024-04-03 13:59:26 -0600

    pagina moderna

---
+++ WorkFolder/IngSoftware/Practica2_6/index.js
@@ -0,0 +1,3 @@
+document.getElementById("btnSiguientePagina").addEventListener("click", () => {
+    window.location.href = "./slide2/slide2.html";
+});
---
+++ WorkFolder/IngSoftware/Practica2_6/slide2/sd.js
@@ -0,0 +1,4 @@
+let frutas = ["Manzana", "Banana", "Cereza"];
+let frutaEliminada = frutas.shift();
+console.log(frutas); // Output: ["Banana", "Cereza"]
+console.log(frutaEliminada); // Output: "Manzana"
